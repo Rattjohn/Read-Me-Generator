320d3ac69aea5780741cc414bac415afaa078bfa
ian rattigan <ian.rattigan2000@gmail.com>
2023-06-19 17:26:15 -0400

    replace my switch case with and if else

--- Develop/index.js
+++ Develop/index.js
@@ -1,75 +1,95 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const inquirer=require("inquirer")
-const fs=require("fs")
+const inquirer = require("inquirer")
+const fs = require("fs");
+const { default: Choice } = require("inquirer/lib/objects/choice");
+const { default: Choices } = require("inquirer/lib/objects/choices");
 
 
 
 
 inquirer.prompt([
     {
-        type:"input",
-        name:"projectTitle",
-        message:"What is the name of your Project"
+        type: "input",
+        name: "projectTitle",
+        message: "What is the name of your Project"
     },
     {
-        type:"input",
-        name:"projectDescription",
-        message:"Please describe what your application does"
+        type: "input",
+        name: "projectDescription",
+        message: "Please describe what your application does"
     },
     {
-        type:"input",
-        name:"projectDescription2",
-        message:"Please describe why you used the technologies you used"
+        type: "input",
+        name: "projectDescription2",
+        message: "Please describe why you used the technologies you used"
     },
     {
-        type:"input",
-        name:"projectDescription3",
-        message:"What were some of the challenges you faces and features you hope to impelement"
+        type: "input",
+        name: "projectDescription3",
+        message: "What were some of the challenges you faces and features you hope to impelement"
     },
     {
-        type:"input",
-        name:"table",
-        message:"Write down your table of contents(optional)"
+        type: "input",
+        name: "table",
+        message: "Write down your table of contents(optional)"
     },
     {
-        type:"input",
-        name:"install",
-        message:"how do you install and run your project?"
+        type: "input",
+        name: "install",
+        message: "how do you install and run your project?"
     },
     {
-        type:"input",
-        name:"projectLicense",
-        message:"add the current license you are using "
+        type: "list",
+        name: "projectLicense",
+        message: "add the current license you are using ",
+        choices: ["Apache License 2.0", "GNU General Public License", "MIT License", "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal"]
     },
     {
-        type:"input",
-        name:"badges",
-        message:"add your badge(if used) "
+        type: "input",
+        name: "badges",
+        message: "add your badge(if used) "
     },
     {
-        type:"input",
-        name:"git",
-        message:"What is your github username "
+        type: "input",
+        name: "git",
+        message: "What is your github username "
     },
     {
-        type:"input",
-        name:"email",
-        message:"What is your email and how can i contact you "
+        type: "input",
+        name: "email",
+        message: "What is your email and how can i contact you "
     }
-]).then(response=>{
-    const {projectTitle}=response;
-    const {projectDescription}=response;
-    const {projectDescription2}=response;
-    const {projectDescription3}=response;
-    const {table}=response;
-    const {install}=response;
-    const {projectLicense}=response;
-    const {badges}=response;
-    const {git}=response;
-    const {email}=response;
-    const README_TEMP =`# <${projectTitle}>
+]).then(response => {
+    const { projectTitle } = response;
+    const { projectDescription } = response;
+    const { projectDescription2 } = response;
+    const { projectDescription3 } = response;
+    const { table } = response;
+    const { install } = response;
+    const { projectLicense } = response;
+    const { badges } = response;
+    const { git } = response;
+    const { email } = response;
+
+    let licenseLink;
+
+    if (projectLicense === "Apache License 2.0") {
+        licenseLink = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    } else if (projectLicense === "GNU General Public License") {
+        licenseLink = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    } else if (projectLicense === "MIT License") {
+        licenseLink = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    } else if (projectLicense === "Boost Software License 1.0") {
+        licenseLink = "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+    } else if (projectLicense === "Creative Commons Zero v1.0 Universal") {
+        licenseLink = "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)";
+    } else {
+        // handle default case
+    }
+
+    const README_TEMP = `# <${projectTitle}>
 
 ## Description
 ${projectDescription}
@@ -84,7 +104,7 @@ ${table}
 - ${install}
 
 ## License
-${projectLicense}
+${licenseLink}
 
 ## Badges
 ${badges}
@@ -94,8 +114,8 @@ https://github.com/${git}
 ${email}`;
 
 
-    fs.writeFile("README.md",README_TEMP,error=>{
-        if(error) {
+    fs.writeFile("README.md", README_TEMP, error => {
+        if (error) {
             console.log(error);
         } else {
             console.log("Read me file has been created");
